ac36488dbcc39dda4dd03f4942d5e6ca1aafb7ee
pnaulls <peter@chocky.org>
2018-05-16 11:55:43 -0700

    Flush code, version 1

--- orenda/flush.ino
+++ orenda/flush.ino
@@ -27,6 +27,8 @@ static bool pump1Finished = false; // No more water from reservior
 
 static lcDirection direction = lcDirectionUnknown;
 static unsigned long lastWater = 0;
+static unsigned long lastEven  = 0;
+static unsigned long lastPump2 = 0;
 static double lcLastValue = 0;
 
 
@@ -49,7 +51,7 @@ static int flushWater(String command) {
     
     heaterAndPumpsOff();
     neverSawWater = true;
-    lastWater = millis();
+    lastWater = lastEven = lastPump2 = millis();
      
     runState = orendaFlush;
     direction = lcDirectionUnknown;
@@ -68,45 +70,89 @@ void flushProcess(bool chamberF) {
   unsigned int now = millis();
   double lcValue = loadCell(""); 
   
+  
+  
+  
+  Particle.publish("flush/chamber", chamberF ? "full" : "empty");
+  
   if ((lcLastValue - lcValue) > lcThreshold) {
       // Decreasing
       direction = lcDirectionDown;
-  } else if ((lcLastValue - lcValue) > -lcThreshold) {
+      // Reset comparison
+      lcLastValue = lcValue;
+  } else if ((lcLastValue - lcValue) < -lcThreshold) {
       // Increasing 
       direction = lcDirectionUp;
+      // Reset comparison
+      lcLastValue = lcValue;
+      lastWater = now;
   } else {
+      if (direction != lcDirectionEven) {
+          lastEven = now;
+      }
+      
+      // We want the load cell to be unchanging for 8 seconds as an exit condition
       direction = lcDirectionEven;
   }
   
+  
+  
+  Particle.publish("flush/direction", String(lcValue) + " "  + String(direction));
+  
   // Always run pump 3
   digitalWrite(pump3, HIGH);
+  
+  // Note that because pump2 is continually draining, the water sensor
+  // might not get triggered.   For this reason, if the direction is up, also
+  // set lastWater above.
   
   if (chamberF) {
       // Definitely water there in chamber
       neverSawWater = false;   
       lastWater = now;
+      
+      if (pump1Finished) {
+         pump1Finished = false;
+         Particle.publish("flush/pump1", "water again");
+      }
+      
   } else {
       // Run for at least 40 seconds.  
             
       if ((now - lastWater) > (40 * 1000)) {
          // Expired
+         Particle.publish("flush/pump1", "now finished");
          pump1Finished = true;
       }
   }
    
-  digitalWrite(pump1, !pump1Finished);
-
-  // Run pump2 if we saw water in the last 40 seconds
+ 
   
-  if ((now - lastWater) > (40 * 1000)) {
-     digitalWrite(pump2, LOW);  
+  Particle.publish("flush/pump1", pump1Finished ? "finished" : "not finished");
+  digitalWrite(pump1, pump1Finished ? LOW : HIGH);
+
+  // Run pump2 if we saw water in the last 40 seconds, and the load cell is not 
+  // increasing
+  
+  if ((direction != lcDirectionUp) && (now - lastWater) < (40 * 1000)) {
+      Particle.publish("flush/on2/lastWater", String(now - lastWater)); 
+      digitalWrite(pump2, HIGH);  
+      lastPump2 = now;
   } else {
-     digitalWrite(pump2, HIGH);   
+      Particle.publish("flush/off2/lastWater", String(now - lastWater)); 
+      digitalWrite(pump2, LOW);   
   }    
   
+  
+  // Consider finish
+  if (!chamberF && direction == lcDirectionEven && pump1Finished) {
+     if (((now - lastEven) > 8000) && ((now - lastPump2) > 8000)) {
+        Particle.publish("flush/finish", "flush complete");
+        powerOff("");
+     }
+  }
 }
  
  
- 
- 
+
  
